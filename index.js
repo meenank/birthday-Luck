5f3ff6136ad74673750de0b1d0776db514112743
Varun Gungi <54449583+meenank@users.noreply.github.com>
2022-01-04 19:59:06 +0530

    wired successfully

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const dateIn = document.querySelector("#dateinp");
 const luckIn = document.querySelector("#luckynum");
-const subBtn = document.querySelector("#btnsub");
+const subBtn = document.querySelector("#btnSub");
+const txtArea = document.querySelector("op-styling")
+
+subBtn.addEventListener("click", function resp() {
+
+    const dob = dateIn.value;
+    // console.log(dob);
+
+    const luckNum = luckIn.value;
+    // console.log(luckNum);
+
+
+})
