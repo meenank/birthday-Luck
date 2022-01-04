75810167fda620f2ace9fe53294a33b89eede930
Varun Gungi <54449583+meenank@users.noreply.github.com>
2022-01-04 21:57:05 +0530

    finished full styling

--- index.js
+++ index.js
@@ -5,11 +5,26 @@ const txtArea = document.querySelector("op-styling")
 
 subBtn.addEventListener("click", function resp() {
 
-    const dob = dateIn.value;
-    // console.log(dob);
+    var dob = dateIn.value;
+
 
     const luckNum = luckIn.value;
     // console.log(luckNum);
+    var sum = 0;
+    dob = dob.replaceAll("-", "");
+
+    for (var i = 0; i < dob.length; i++) {
+
+        sum += Number(dob.charAt(i));
+    }
+
+    console.log("sum=", sum)
+    console.log("rem=", sum % luckNum);
+    if (sum % luckNum == 0) {
+        txtArea.value = "Yay, your birthday's lucky 🥳";
+    } else {
+        txtArea.value = "darn it, guess you have to create your own luck! 😉";
+    }
 
 
 })
